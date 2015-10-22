6ffa0fea7b816fe20c1a92dfbacfae53746f160a
Stephen Larsen <maxpendragon@gmail.com>
2015-10-22 16:04:34 -0600

    Updated current Dux label.

---
+++ BanyaProject/Assets/DuxBalance.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class DuxBalance : MonoBehaviour {
+
+    public Text accountBalance;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        accountBalance.text = CurrencySystem.Get().ToString();
+	}
+}
