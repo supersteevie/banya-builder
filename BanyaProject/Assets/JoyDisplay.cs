d3a7c5e65c3611b1458f52323cc6482f46eaac97
Stephen Larsen <maxpendragon@gmail.com>
2015-10-22 01:41:04 -0600

    Updated GUI. It looks so sexy it hurts.

---
+++ BanyaProject/Assets/JoyDisplay.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class JoyDisplay : MonoBehaviour {
+
+	public Text joyText;
+	public Image joyEmoji;
+	public Sprite[] emoji;
+	public Color bad;
+	public Color great;
+
+	private float joyStats;
+	private float colorChange;
+
+	// Use this for initialization
+	void Start () {
+		joyStats = BanyaRating.satisfaction;
+		joyText.text = (int)joyStats + "%";
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		BanyaRating.HappyKimans();
+		JoyEmojiColor();
+	}
+
+	void JoyEmojiColor () {
+		joyStats = BanyaRating.satisfaction;
+		//colorChange = joyStats * 0.01f;
+		joyText.text = (int)joyStats + "%";
+
+	}
+}
