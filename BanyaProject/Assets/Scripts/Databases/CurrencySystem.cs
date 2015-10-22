6bb4737ecfce87998fe9dc1d522facdc657af849
505 Computer A <zachauna@gmail.com>
2015-10-22 15:57:59 -0600

    -CurrencySystem

---
+++ BanyaProject/Assets/Scripts/Databases/CurrencySystem.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public static class CurrencySystem {
+
+	private static int currentDux = 2000;
+
+	public  static void Set(int income)
+	{
+		currentDux += income;
+	}
+
+	public static int Get()
+	{
+		return currentDux;
+	}
+
+	public static bool EnoughDux(int cost)
+	{
+		if (cost > currentDux)
+			return false;
+		else
+			return true;
+	}
+
+}
